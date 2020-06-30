bd6779f044865a44ad3e71807926b9de275f2a8b
Trent Schneider <trentonschneider@gmail.com>
2020-06-30 15:31:09 -0400

    started working on pulling data and css

---
+++ Assets/script.js
@@ -0,0 +1,27 @@
+var long, lat;
+
+// if (navigator.geolocation) {
+//   navigator.geolocation.getCurrentPosition(showPosition);
+//   function showPosition(position) {
+//     lat = position.coords.latitude;
+//     long = position.coords.longitude;
+//     console.log(lat);
+//     console.log(long);
+//   }
+// }
+// function getLocalWeather() {
+//   $.ajax({
+//     url:
+//       "https://api.openweathermap.org/data/2.5/weather?lat=" +
+//       lat +
+//       "&lon=" +
+//       long +
+//       "&appid=366cc20005f64d752fb214b02121c288",
+//     method: "GET",
+//   }).then(function (response) {
+//     console.log(response);
+//   });
+// }
+// function showData(){
+
+// }
