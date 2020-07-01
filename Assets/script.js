4ed77c8196bf6818711f83da1bb7eb8a4525cd5d
Trent Schneider <trentonschneider@gmail.com>
2020-06-30 23:44:32 -0400

    set up weather calls and worked on getting list appending

--- Assets/script.js
+++ Assets/script.js
@@ -1,31 +1,60 @@
 var long, lat;
-var newLocation;
-// if (navigator.geolocation) {
-//   navigator.geolocation.getCurrentPosition(showPosition);
-//   function showPosition(position) {
-//     lat = position.coords.latitude;
-//     long = position.coords.longitude;
-//     console.log(lat);
-//     console.log(long);
-//   }
-// }
-// function getLocalWeather() {
-//   $.ajax({
-//     url:
-//       "https://api.openweathermap.org/data/2.5/weather?lat=" +
-//       lat +
-//       "&lon=" +
-//       long +
-//       "&appid=366cc20005f64d752fb214b02121c288",
-//     method: "GET",
-//   }).then(function (response) {
-//     console.log(response);
-//   });
-// }
-// function showData(){
-// }
+var newLocation, cityName, cityTemp, cityHumid, cityWind, cityUV, weatherData;
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(showPosition);
+  function showPosition(position) {
+    lati = position.coords.latitude;
+    long = position.coords.longitude;
+  }
+}
+function getWeather() {
+  $.ajax({
+    url:
+      "https://api.openweathermap.org/data/2.5/weather?lat=" +
+      lati +
+      "&lon=" +
+      long +
+      "&appid=366cc20005f64d752fb214b02121c288&units=imperial",
+    method: "GET",
+  }).then(function (response) {
+    $("#cityName").empty();
+    $("#cityName").text(response.name);
+    $("#currentTemp").empty();
+    $("#currentTemp").text("Temperature: " + response.main.temp);
+    $("#currentHumidity").empty();
+    $("#currentHumidity").text("Humidity: " + response.main.humidity);
+    $("#windSpeed").empty();
+    $("#windSpeed").text("Wind Speed: " + response.wind.speed);
+  });
+  $.ajax({
+    url:
+      "http://api.openweathermap.org/data/2.5/uvi?appid=366cc20005f64d752fb214b02121c288&lat=" +
+      lati +
+      "&lon=" +
+      long,
+    method: "GET",
+  }).then(function (response) {
+    $("#uv").empty();
+    $("#uv").text("UV Index: " + response.value);
+  });
+}
+$("#testButton").on("click", function () {
+  getWeather();
+});
+function showData() {}
 $("#newLocationBtn").on("click", function () {
-  var newLocation = $("#citySearch").val;
-  var newLocationDiv = $("locationList").append("<div>");
-  newLocationDiv.text(newLocation);
+  var newLocation = $("#citySearch").val();
+  var newLocationLI = $("#locationList").append("<li>");
+  newLocationLI.text(newLocation);
+  $.ajax({
+    url:
+      "http://api.openweathermap.org/data/2.5/weather?q=" +
+      newLocation +
+      "&appid=366cc20005f64d752fb214b02121c288",
+    method: "GET",
+  }).then(function (response) {
+    lati = response.coord.lat;
+    long = response.coord.lon;
+    getWeather();
+  });
 });
